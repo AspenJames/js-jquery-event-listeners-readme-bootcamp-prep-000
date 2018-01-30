874e212253216379e890604d64e55ec1ab3f7fbc
Aspen James <aspenjames@tqca.org>
2018-01-30 04:22:45 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,7 +20,7 @@ function frameIt(){
 
 function pressIt(){
   $("#typing").on("keypress", function(e){
-    if(parseInt(e.which) == 71){
+    if(parseInt(e.detail || e.which) == 71){
       alert("You have pressed G.");
     }
   })
